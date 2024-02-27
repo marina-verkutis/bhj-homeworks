715760987d62ec509b71de901b180becf32c0ac4
Marina Verkutis <marina.corpwork@gmail.com>
2024-02-27 21:47:21 +0300

    2.3 Task 1

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,24 @@
+const links = Array.from(document.querySelectorAll('.has-tooltip'));
+const tooltip = document.createElement('div');
+
+tooltip.className = 'tooltip';
+
+links.forEach(elem => {
+
+    elem.addEventListener('click', event => {
+        event.preventDefault();
+        target = event.target;
+
+        if(this.target.title === tooltip.textContent){
+            tooltip.classList.toggle('tooltip_active');
+        } else  tooltip.classList.add('tooltip_active')
+
+        
+        tooltip.innerText = this.target.title;
+        const { bottom, left } = target.getBoundingClientRect();
+        tooltip.style = `left: ${left}px; top: ${bottom}px`;
+        target.insertAdjacentElement('afterEnd', tooltip);
+        
+    })
+
+})
