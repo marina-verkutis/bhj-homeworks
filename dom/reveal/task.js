e5f099f36ee0eb444b09a7cb7fd458f6ec85ede9
Marina Verkutis <marina.corpwork@gmail.com>
2024-02-27 21:34:04 +0300

    1.4 Task 1

---
+++ dom/reveal/task.js
@@ -0,0 +1,12 @@
+const revealBlock = Array.from(document.querySelectorAll(".reveal"));
+
+window.addEventListener("scroll", (el) => {
+    revealBlock.forEach((el) => {
+        const {top, bottom} = el.getBoundingClientRect();
+        if ((bottom < 0) || (top > window.innerHeight)) {
+            el.classList.remove("reveal_active");
+        } else {
+            el.classList.add("reveal_active");
+        }
+    })
+})
